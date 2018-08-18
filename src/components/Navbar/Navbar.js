114e9f4594a9ca24eb0cc8f3d2cf0b78443711b1
Wanchalerm <tickstudiu@gmail.com>
2018-08-18 18:43:43 +0700

    Add dropdown in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Nav, NavLink, Badge } from 'reactstrap';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faSlidersH, faCommentDots } from '@fortawesome/free-solid-svg-icons'
+import React, {Component} from 'react';
+import {Nav, NavLink, Badge, Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBell, faSlidersH, faCommentDots} from '@fortawesome/free-solid-svg-icons'
 import styled from 'styled-components';
 
 library.add(faBell, faSlidersH, faCommentDots);
@@ -12,20 +12,52 @@ const EmptyBox = styled.div`
   width: 0;
 `;
 
-class Navbar extends Component{
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            dropdownOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState(prevState => ({
+            dropdownOpen: !prevState.dropdownOpen
+        }));
+    }
+
+
     render() {
         return (
             <Nav className="justify-content-end align-items-center shadow">
                 <NavLink><EmptyBox/></NavLink>
 
-                <NavLink href="#" className="border rounded-circle position-relative mr-3 text-dark">
-                    <FontAwesomeIcon icon="comment-dots"/>
-                    <Badge color="danger" className="position-absolute" style={{top: '-5px',right: '-5px'}}>1</Badge>
-                </NavLink>
+
+                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+
+                    <DropdownToggle className="bg-transparent border-0 p-0">
+
+                        <NavLink href="#" className="border rounded-circle position-relative mr-3 text-dark">
+                            <FontAwesomeIcon icon="comment-dots"/>
+                            <Badge color="danger" className="position-absolute" style={{top: '-5px', right: '-5px'}}>1</Badge>
+                        </NavLink>
+
+                    </DropdownToggle>
+
+                    <DropdownMenu right className="rounded-0">
+                        <DropdownItem header>Header</DropdownItem>
+                        <DropdownItem disabled>Action</DropdownItem>
+                        <DropdownItem>Another Action</DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>Another Action</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
 
                 <NavLink href="#" className="border rounded-circle position-relative mr-3 text-dark">
                     <FontAwesomeIcon icon="bell"/>
-                    <Badge color="danger" className="position-absolute" style={{top: '-5px',right: '-5px'}}>4</Badge>
+                    <Badge color="danger" className="position-absolute" style={{top: '-5px', right: '-5px'}}>4</Badge>
                 </NavLink>
 
                 <NavLink href="#" className="border rounded-circle position-relative mr-3 text-dark">
