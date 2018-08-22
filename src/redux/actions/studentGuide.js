a24376d6134fca6522d5ba3ba79b394dabefbc19
Wanchalerm <tickstudiu@gmail.com>
2018-08-22 17:37:29 +0700

    Try to fetch data form mysql database and change action to anyc function

---
+++ src/redux/actions/studentGuide.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import { FETCH_STUDENT_GUIDE } from './type';
+
+const URL ='http://localhost:5000/api/student-guide';
+
+export async function fetchStudentGuide(){
+    const response = await axios.get(`${URL}`);
+    return {
+        type:FETCH_STUDENT_GUIDE,
+        payload: response
+    }
+}
